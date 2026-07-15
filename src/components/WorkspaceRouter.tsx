c5048278e48c146d3a898b191830e8712210f889
mishattsoy-netizen <mishattsoy@gmail.com>
2026-07-15 19:41:24 +0300

    fix(refresh): seed WorkspaceRouter from initialEntityId cookie

--- src/components/WorkspaceRouter.tsx
+++ src/components/WorkspaceRouter.tsx
@@ -27,7 +27,7 @@ export const WorkspaceRouter = memo(function WorkspaceRouter({ initialEntityId }
       ref={containerRef}
       className="flex-1 flex flex-col min-h-0 relative"
     >
-      <EntityPageRenderer entityId={activeEntityId ?? 'dashboard'} />
+      <EntityPageRenderer entityId={activeEntityId ?? initialEntityId ?? 'dashboard'} />
     </div>
   );
 });
